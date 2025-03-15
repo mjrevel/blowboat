905b59c65e87543dde74d60cb6ed4b84c577d398
Michael Revel <mjrevel@gmail.com>
2025-03-15 14:13:26 -0400

    serial com added, not calibrated

--- ocean.gd
+++ ocean.gd
@@ -1,1 +1,26 @@
 extends Node3D
+
+var serial
+var recv
+
+func _ready():
+	# List all connected serial devices.
+	var devices = Serial.list_ports()
+	print(devices)
+
+	serial = Serial.new()
+	# Open by vender id and product id
+	serial.open("/dev/pts/1", 115200)
+
+	if serial.is_open() == true:
+		# Then you can read data from serial device.
+		recv = serial.read()
+	else:
+		serial.close()
+	# And write report data to serial device
+	#serial.write(data_to_send)
+
+
+func _process(delta):
+	if serial.is_open() == true:
+		recv = serial.read()
